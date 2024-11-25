496e2c36ebca0c2cdb9eb17b5389eb0c6edda84c
uueak <stefan.scheiffele@student.kit.edu>
2024-11-25 17:23:07 +0100

    - The correct window to connect to is now selected automatically.

--- accessibleAdrift.py
+++ accessibleAdrift.py
@@ -1,21 +1,32 @@
 from pywinauto import Application
 from pywinauto.keyboard import send_keys
+from pywinauto import Desktop
 import time
 import os
 import shutil
 import textwrap
 
 def main():
-    while True:
-        title = input("Please enter the title of your game as shown in the FrankenDrift window. Upper- and lower case matters. For example, if the title bar says \"My Game - FrankenDrift\", you have to type \"My Game\"\ntitle: ")
-        if title == "quit":
-            return
-        try:
-            app = Application().connect(title=title + " - FrankenDrift")
-            break
-        except:
-            print("An error occured. Please try again.")
-    main_window = app.window(title=title + " - FrankenDrift")
+    windows = Desktop(backend="uia").windows()
+    target = None
+    for win in windows:
+        if win.window_text().endswith(" - FrankenDrift"):
+            target = win.window_text()
+    if target:
+        app = Application().connect(title=target)
+    else:
+        input("No FrankenDrift window found. Please open your game first, then try again. You have to restart this script.\nPress Enter to quit.")
+        return
+#    while True:
+#        title = input("Please enter the title of your game as shown in the FrankenDrift window. Upper- and lower case matters. For example, if the title bar says \"My Game - FrankenDrift#\", you have to type \"My Game\"\ntitle: ")
+#        if title == "quit":
+#            return
+#        try:
+#            app = Application().connect(title=title + " - FrankenDrift")
+#            break
+#        except:
+#            print("An error occured. Please try again.")
+    main_window = app.window(title=target)
     controls = main_window.descendants()
     inputField = controls[4]
     outputField = controls[3]
